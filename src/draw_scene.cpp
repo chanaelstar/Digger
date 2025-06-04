baa5bafedfefb86e2073adc2e53c410525cc90ad
Alice <alice33.jak@gmail.com>
2025-06-04 13:24:01 +0200

    key callback fermeture de fenetre

--- src/draw_scene.cpp
+++ src/draw_scene.cpp
@@ -332,6 +332,11 @@ bool canMoveSquare(float x, float y, float playerSize, const std::vector<std::ve
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
+
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+        return;
+    }
     if(victory) return;
 
     if (action == GLFW_PRESS || action == GLFW_REPEAT)
